fc12bd5f303b791b7519efa012300729bb04bc16
Florent Rougon <f.rougon@free.fr>
2015-01-11 17:48:40 +0100

    Release 1.0.0rc1

--- condconfigparser/version.py
+++ condconfigparser/version.py
@@ -46,7 +46,7 @@ class VersionInfo(_VersionInfo):
     def __repr__(self):
         return "{0}.{1}".format(__name__, _VersionInfo.__repr__(self))
 
-version_info = VersionInfo(0, 9, 0, "b1.dev3")
+version_info = VersionInfo(1, 0, 0, "rc1")
 __version__ = str(version_info)
 
 del collections
