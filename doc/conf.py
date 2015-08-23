e44883f57ef59f191d2a9c0091d078928e98fe63
Florent Rougon <f.rougon@free.fr>
2015-08-23 12:22:56 +0200

    Set needs_sphinx = '1.3' and html_theme = 'classic' for the doc
    * This is to cope with the change of the default theme in Sphinx 1.3.

--- doc/conf.py
+++ doc/conf.py
@@ -24,7 +24,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '1.3'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -108,7 +108,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
